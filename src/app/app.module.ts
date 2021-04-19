2e5d4b5f4130f25a8975f8f126a30958dc838cfa
redsuperbat <max.netterberg@gmail.com>
2021-04-19 11:33:25 +0200

    added tauri

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FfmpegService } from './services/ffmpeg.service';
 import { UiElementsModule } from './ui-elements/ui-elements.module';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, UiElementsModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    UiElementsModule,
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the app is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
+  ],
   providers: [FfmpegService],
   bootstrap: [AppComponent],
 })
--- src/app/ui-elements/dropdown/dropdown.component.ts
+++ src/app/ui-elements/dropdown/dropdown.component.ts
@@ -11,6 +11,7 @@ import {
 } from '@angular/core';
 import { BehaviorSubject, Observable, Subject, Subscription } from 'rxjs';
 import { map, startWith } from 'rxjs/operators';
+import { serialUnsubscriber, SubscriptionCollection } from 'src/app/utils';
 
 @Component({
   selector: 'app-dropdown',
@@ -18,7 +19,9 @@ import { map, startWith } from 'rxjs/operators';
   styleUrls: ['./dropdown.component.scss'],
 })
 export class DropdownComponent<T> implements OnInit, OnDestroy {
-  private subs: Subscription[] = [];
+  private subs: SubscriptionCollection = {};
+
+  public onCloseClick$ = new Subject<void>();
   public onDropdownClick$ = new Subject<MouseEvent>();
   public isVisible$ = new BehaviorSubject(false);
   public onDropdownItemClick$ = new Subject<T>();
@@ -41,26 +44,31 @@ export class DropdownComponent<T> implements OnInit, OnDestroy {
       }
     });
 
-    this.subs.push(
-      this.onDropdownClick$.subscribe(() => {
-        this.isVisible$.next(!this.isVisible$.value);
-      }),
-      this.onDropdownItemClick$.subscribe((item: T) => {
+    this.subs.dropdownClick = this.onDropdownClick$.subscribe(() => {
+      this.isVisible$.next(!this.isVisible$.value);
+    });
+    this.subs.dropdownItemClick = this.onDropdownItemClick$.subscribe(
+      (item: T) => {
         this.isVisible$.next(false);
-        this.onSelect?.emit(item);
-      })
+        this.onSelect.emit(item);
+      }
     );
+    this.subs.closeClick = this.onCloseClick$.subscribe(() =>
+      this.onDropdownItemClick$.next(undefined)
+    );
+
     this.selectedDropdownItemName$ = this.onDropdownItemClick$.pipe(
       map((item) => {
+        if (item === null || item === undefined) return this.initalText;
         if (this.labelField) return String(item[this.labelField]);
-        return this.initalText;
+        return String(item);
       }),
       startWith(this.initalText)
     );
   }
 
   ngOnDestroy(): void {
-    this.subs.forEach((s) => s.unsubscribe());
+    serialUnsubscriber(this.subs);
   }
   @Input()
   public items: T[];
